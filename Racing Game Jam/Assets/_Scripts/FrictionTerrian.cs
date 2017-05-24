eafba078cdbd81232f6d19cd74888955780b1704
Hameed Abdul <hameedabdulrashid@gmail.com>
2017-05-24 15:25:51 -0500

    Curved based movement

---
+++ Racing Game Jam/Assets/_Scripts/FrictionTerrian.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionTerrian : MonoBehaviour
+{
+    Car driver;
+
+    void OnTriggerEnter(Collider other)
+    {
+        driver = other.GetComponent<Car>();
+        Debug.Log(driver.Speed);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        driver = other.GetComponent<Car>();
+        driver.Speed = 10;
+        Debug.Log(driver.Speed);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        driver = other.GetComponent<Car>();
+        driver.Speed = 1;
+        Debug.Log(driver.Speed);
+    }
+}
