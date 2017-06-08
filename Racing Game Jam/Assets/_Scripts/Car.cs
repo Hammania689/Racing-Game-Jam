ed0a1dcfa466e7803efc69fb22a5fe5490c8f6ea
Hameed Abdul <hameedabdulrashid@gmail.com>
2017-06-08 01:59:32 -0500

    Proper dictionary implementation. Transform need to be adjusted accordingly as well number of prefabs instantiated...

---
+++ Racing Game Jam/Assets/_Scripts/Car.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float Speed = 2.2f;
+
+    float time = 0;
+    Rigidbody2D rb;
+    Quaternion currentRot;
+
+    Vector3 movement;
+    Vector3 curve;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movement = Vector3.up;
+        curve = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Cos(time);
+        float y = Mathf.Sin(time);
+        float z = 0f;
+
+        Debug.DrawRay(transform.localPosition,Vector3.up + curve,Color.green);
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            curve = new Vector3(x, y, z);
+            time += Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0,0,Mathf.Acos(time) * 180);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            curve = new Vector3(x, y, z);
+            time += Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, -Mathf.Acos(time) * 180);
+        }
+        else if(transform.rotation == Quaternion.identity)
+        {
+            time = 0;
+        }
+
+        rb.velocity =  transform.rotation * movement * Speed;
+    }
+}
---
+++ Racing Game Jam/Assets/_Scripts/RoadGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+    public GameObject[] road = new GameObject[2];
+    public int count = 4;
+
+    int randomPeice;
+    Quaternion spawnRot;
+    Road spawningPeice, prevPeice;
+    Vector3 spawnPos;
+
+    Dictionary<int, Road> currRoad = new Dictionary<int, Road>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        spawnPos = new Vector3(0,0,1);
+
+        for (int i = 0; i < count; i++)
+        {
+            // Selects any 1 of the 3 possible peices
+            randomPeice = Mathf.RoundToInt(Random.Range(0, 3));
+
+            // Sets the random number to the correlating array index prefab
+            spawningPeice = road[randomPeice].GetComponent<Road>();
+
+            // Adds the current peice to the Dictionary
+            currRoad.Add(i, spawningPeice);
+
+            // Spawns the Current peice
+            if(i >= 1)
+            {
+                // Stores previous dictionary element in variable
+                prevPeice = currRoad[i - 1];
+                spawnRot = prevPeice.GetSpawnRotation(prevPeice);
+                spawnPos = prevPeice.GetSpawnZone();
+            }
+
+            // Spawns the road accordingly
+            Instantiate(road[randomPeice] as GameObject, spawnPos, spawnRot, this.gameObject.transform);
+
+            //currRoad[i] = spawningPeice;
+            spawnPos = spawningPeice.GetSpawnZone() + spawningPeice.transform.position;
+
+        }
+    }
+}
